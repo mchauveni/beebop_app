70f26b77412f6912f278479779b7f477e0697630
LATREMOLIERE ALEX <alatremo@iut.local>
2023-03-07 16:08:56 +0100

    init branch front

--- public/index.php
+++ public/index.php
@@ -7,3 +7,17 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beebop</title>
+</head>
+<body>
+    <header>Beebop</header>
+    <main class="glass"></main>
+</body>
+</html>
