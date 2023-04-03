a1379ceb85a6c6624dd4cb7429e5440ace577330
GARRETA SOPHIE <sophie.garreta@gmail.com>
2023-04-03 18:24:48 +0200

    beekeeper crud profile

--- src/Controller/BeekeeperController.php
+++ src/Controller/BeekeeperController.php
@@ -19,8 +19,16 @@ class BeekeeperController extends AbstractController
     #[Route('/', name: 'app_beekeeper_index', methods: ['GET'])]
     public function index(BeekeeperRepository $beekeeperRepository): Response
     {
-        return $this->render('beekeeper/index.html.twig', []);
+        $beekeeper = $this->getUser();
+        
+        return $this->render('beekeeper/index.html.twig', [
+            'beekeeper' => $beekeeper,
+        ]);
     }
+
+    /*
+    *  START route for ADMIN
+    */
 
     #[Route('/admin', name: 'admin_dashboard')]
     public function index_admin(
@@ -33,6 +41,22 @@ class BeekeeperController extends AbstractController
             'beekeepers' => $beekeepers,
         ]);
     }
+
+    #[Route('/admin/{id}', name: 'admin_beekeeper_show')]
+    public function showBeekeeper_admin(
+        Beekeeper $beekeeper
+    ): Response {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        // récupérer tous les apiculteurs
+        $beekeeper = $beekeeper;
+        return $this->render('admin/show.html.twig', [
+            'beekeeper' => $beekeeper,
+        ]);
+    }
+
+    /*
+    *  END route for ADMIN
+    */
 
     #[Route('/{id}/edit', name: 'app_beekeeper_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Beekeeper $beekeeper, BeekeeperRepository $beekeeperRepository): Response
@@ -52,7 +76,16 @@ class BeekeeperController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_beekeeper_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_beekeeper_show', methods: ['GET', 'POST'])]
+    public function show(Request $request, Beekeeper $beekeeper, BeekeeperRepository $beekeeperRepository): Response
+    {
+        $beekeeper = $this->getUser();
+        return $this->render('beekeeper/show.html.twig', [
+            'beekeeper' => $beekeeper,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_beekeeper_delete', methods: ['POST'])]
     public function delete(Request $request, Beekeeper $beekeeper, BeekeeperRepository $beekeeperRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $beekeeper->getId(), $request->request->get('_token'))) {
